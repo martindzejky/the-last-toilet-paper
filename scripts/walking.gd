62478fb44db65e83924cd033e173d054854cd121
Martin Jakubik <martin@jakubik.info>
2020-05-16 15:48:36 +0200

    remove print

--- scripts/walking.gd
+++ scripts/walking.gd
@@ -17,7 +17,6 @@ func _process(_delta):
 
     # get the vector's angle (in RADIANS!) and use it to set the correct animation
     var angle = positionDelta.angle() + 2*PI
-    print(angle)
 
     # the + 0.1 is there to better handle "perfect" diagonal movement
     if angle < PI*5/4 + 0.05:
